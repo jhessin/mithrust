1726ace94e07c80f10aeba4c8f83c092cce443c7
Jim Hessin <jhessin@gmail.com>
2019-06-10 08:58:30 -0600

    Pulled logic out into the library.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,6 @@
-#[macro_use]
-extern crate stdweb;
+extern crate mithrust;
 
+use mithrust::*;
 fn main() {
-    let message = "Hello, WASM!";
-    let message_node = stdweb::web::document().create_text_node(message);
-    js! {
-        document.body.appendChild(@{message_node})
-    }
+    m("div", "Hello, WASM!");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[macro_use]
+extern crate stdweb;
+
+pub fn m<'a>(el: &str, text: &'a str) {
+    let message_node = stdweb::web::document().create_text_node(text);
+    js! {
+        document.body.appendChild(@{message_node})
     }
 }
