a979099730638fca5821e244016d9a41644e8783
Jim Hessin <jhessin@gmail.com>
2019-06-25 14:43:09 -0600

    Progress ---
    Added a new file to start planning a vnode
    got a working DOM manipulation code

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -1,7 +1,6 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
-
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
 //
@@ -10,25 +9,23 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-  // This provides better error messages in debug mode.
-  // It's disabled in release mode so it doesn't bloat up the file size.
-#[cfg(debug_assertions)]
-  console_error_panic_hook::set_once();
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(debug_assertions)]
+    console_error_panic_hook::set_once();
 
+    // Your code goes here!
+    let window = web_sys::window().expect("No global window!");
+    let document = window.document().expect("No document on window");
+    let body = document.body().expect("Document should have a body");
 
-  // Your code goes here!
-  let window = web_sys::window().expect("No global window!");
-  let document = window.document().expect("No document on window");
-  let body = document.body().expect("Document should have a body");
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust!");
 
-  let val = document.create_element("p")?;
-  val.set_inner_html("Hello from Rust");
+    body.append_child(&val)?;
 
-  body.append_child(&val)?;
-
-  Ok(())
+    Ok(())
 }
